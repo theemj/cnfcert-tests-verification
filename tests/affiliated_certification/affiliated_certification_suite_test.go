1f1acae243bae37782fbf8ea30a0e3f133ea03b5
Emily Johnson <emjohnso@redhat.com>
2021-12-13 12:24:30 -0500

    Removed functions that are not yet necessary and added logging definitions.

--- tests/affiliated_certification/affiliated_certification_suite_test.go
+++ tests/affiliated_certification/affiliated_certification_suite_test.go
@@ -1,6 +1,8 @@
 package affiliated_certification
 
 import (
+	"flag"
+
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
@@ -11,12 +13,15 @@ import (
 	"testing"
 
 	_ "github.com/test-network-function/cnfcert-tests-verification/tests/affiliated_certification/tests"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/config"
 )
 
 func TestAffiliatedCertification(t *testing.T) {
 	_, currentFile, _, _ := runtime.Caller(0)
 	configSuite, err := config.NewConfig()
+	_ = flag.Lookup("logtostderr").Value.Set("true")
+	_ = flag.Lookup("v").Value.Set(globalhelper.Configuration.General.LogLevel)
 
 	if err != nil {
 		fmt.Print(err)
